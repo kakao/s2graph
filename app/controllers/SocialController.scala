e4ee88c3d02e7ec89b898f16c41caac76571af89
SteamShon <shom83@gmail.com>
2015-09-22 12:26:21 +0900

    rename like controller to social controller

--- app/controllers/SocialController.scala
+++ app/controllers/SocialController.scala
@@ -20,7 +20,7 @@ import play.api.mvc.{Action, Controller}
 import scala.concurrent.Future
 import scala.util.hashing.MurmurHash3
 
-object LikeController extends Controller with RequestParser {
+object SocialController extends Controller with RequestParser {
 
   import scala.concurrent.ExecutionContext.Implicits.global
   import ApplicationController._
@@ -32,41 +32,7 @@ object LikeController extends Controller with RequestParser {
 
   def notAllowedActionTypeException(actionType: String) = new RuntimeException(s"not allowd action type: $actionType")
 
-  val cacheTTL = 5000
-  val filter = CacheBuilder.newBuilder()
-    .expireAfterWrite(cacheTTL, TimeUnit.MILLISECONDS)
-    .maximumSize(10000)
-    .build[Integer, String]()
 
-  /** select */
-  def select(accessToken: String, user: String, actionType: String) = withHeaderAsync(parse.anyContent) { request =>
-    val service = Service.findByAccessToken(accessToken).getOrElse(throw badAccessTokenException(accessToken))
-    val labelName = LikeUtil.userUrlLabels.get(actionType).getOrElse(throw notAllowedActionTypeException(actionType))
-    val serviceId = service.id.get
-    val queryJson = Json.obj("srcVertices" -> Json.arr(Json.obj("serviceName" -> LikeUtil.serviceName, "columnName" -> LikeUtil.srcColumnName, "id" -> user)),
-      "steps" -> Json.arr(
-        Json.obj("step" -> Json.arr(Json.obj("label" -> labelName, "index" -> "IDX_SERVICE_ID",
-          "interval" -> Json.obj("from" -> Json.obj("serviceId" -> serviceId), "to" -> Json.obj("serviceId" -> serviceId))))),
-        Json.obj("step" -> Json.arr(Json.obj("label" -> LikeUtil.urlSelfLabelName, "limit" -> 1)))
-      )
-    )
-    logger.debug(s"$queryJson")
-    QueryController.getEdgesInner(queryJson)
-  }
-
-  def selectAll(user: String, actionType: String) = withHeaderAsync(parse.anyContent) { request =>
-    val labelName = LikeUtil.userUrlLabels.get(actionType).getOrElse(throw notAllowedActionTypeException(actionType))
-
-    val queryJson =
-      Json.obj("srcVertices" -> Json.arr(Json.obj("serviceName" -> LikeUtil.serviceName, "columnName" -> LikeUtil.srcColumnName, "id" -> user)),
-        "steps" -> Json.arr(
-          Json.obj("step" -> Json.arr(Json.obj("label" -> labelName, "index" -> "_PK"))),
-          Json.obj("step" -> Json.arr(Json.obj("label" -> LikeUtil.urlSelfLabelName, "limit" -> 1, "cacheTTL" -> cacheTTL)))
-        )
-      )
-    logger.debug(s"$queryJson")
-    QueryController.getEdgesInner(queryJson)
-  }
 
   /** write */
 
@@ -98,7 +64,7 @@ object LikeController extends Controller with RequestParser {
 
   def like(accessToken: String) = withHeaderAsync(parse.json) { request =>
     val service = Service.findByAccessToken(accessToken).getOrElse(throw badAccessTokenException(accessToken))
-    likeInner(service.id.get)(request.body).map { ret =>
+    likeInner(service)(request.body).map { ret =>
       Ok(s"$ret")
     }
   }
@@ -107,38 +73,38 @@ object LikeController extends Controller with RequestParser {
     val service = Service.findByAccessToken(accessToken).getOrElse(throw badAccessTokenException(accessToken))
 
     val jsVals = request.body.asOpt[Seq[JsValue]].getOrElse(Nil)
-    likesInner(service.id.get)(jsVals).map { rets =>
+    likesInner(service)(jsVals).map { rets =>
       Ok(s"$rets")
     }
   }
 
-  def likesInner(serviceId: Int)(jsVals: Seq[JsValue]): Future[Seq[Boolean]] = {
-    val futures = jsVals.map(likeInner(serviceId)(_))
+  def likesInner(service: Service)(jsVals: Seq[JsValue]): Future[Seq[Boolean]] = {
+    val futures = jsVals.map(likeInner(service)(_))
     Future.sequence(futures)
   }
 
-  def likeInner(serviceId: Int)(jsVal: JsValue): Future[Boolean] = {
+  def likeInner(service: Service)(jsVal: JsValue): Future[Boolean] = {
     val (user, url, actionType) = toParams(jsVal)
-    likeInner(serviceId, user, url, actionType)
+    likeInner(service, user, url, actionType)
   }
 
   /** fire user-url action edge into local queue, and fire scrap request simultaneously */
-  def likeInner(serviceId: Int, user: String,
+  def likeInner(service: Service, user: String,
                 url: String, actionType: String): Future[Boolean] = {
+
     val labelName = LikeUtil.userUrlLabels.get(actionType).getOrElse(throw notAllowedActionTypeException(actionType))
     val ts = System.currentTimeMillis()
     val edgeJson = Json.obj("timestamp" -> ts, "from" -> user, "to" -> url,
-      "label" -> labelName, "props" -> Json.obj("serviceId" -> serviceId))
-    val edge = toEdge(edgeJson, "insert")
+      "label" -> labelName, "props" -> Json.obj("serviceName" -> service.serviceName))
 
-    // fire insert request for edge for (user, url)
-    Logger.info(s"$edge")
-    QueueActor.router ! edge
-    logger.info(s"$QueueActor.scrapeRouter")
+    // publish url to scrape kafka topic so background workers can consume.
     ExceptionHandler.enqueue(KafkaMessage(new ProducerRecord[String, String](Config.KAFKA_SCRAPE_TOPIC, null, url)))
-    //
-    //    UrlScrapeActor.router ! url
-    Future.successful(true)
+
+    for {
+      insertedRet <- EdgeController.tryMutates(edgeJson, "insert")
+    } yield {
+      true
+    }
   }
 
 
