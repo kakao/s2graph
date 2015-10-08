5595a983cf7c051f941b3a5131ac96d8c5ce7030
SteamShon <shom83@gmail.com>
2015-10-08 16:23:52 +0900

    unify result format

--- app/controllers/ScrapeController.scala
+++ app/controllers/ScrapeController.scala
@@ -12,13 +12,16 @@ import scala.concurrent.Future
 import scala.util.hashing.MurmurHash3
 import com.beachape.metascraper.Messages.{ScrapeUrl, ScrapedData}
 import com.beachape.metascraper.Scraper
+
 /**
  * Created by shon on 9/23/15.
  */
 object ScrapeController extends Controller with RequestParser {
+
   import scala.concurrent.ExecutionContext.Implicits.global
   import ApplicationController._
   import actors.KafkaConsumerWithThrottle._
+
   val scraper = new Scraper(Http, Seq("http", "https"))
 
   def scrape(rawUrl: String) = withHeaderAsync(parse.anyContent) { request =>
@@ -28,22 +31,25 @@ object ScrapeController extends Controller with RequestParser {
     import KafkaConsumerWithThrottle._
     val hashKey = MurmurHash3.stringHash(url)
     val oldVal = KafkaConsumerWithThrottle.filter.getIfPresent(hashKey)
-    if (oldVal == null) {
+
+    val data = if (oldVal == null) {
       for {
         scrapedData <- scraper.fetch(ScrapeUrl(url))
         edge = toUrlSelfEdge(url, toShortenUrl(url), scrapedData)
         ret <- Graph.mutateEdgeWithWait(edge)
       } yield {
         KafkaConsumerWithThrottle.filter.put(hashKey, scrapedData)
-        val json = Json.obj("url" -> url, "data" -> toJsObject(scrapedData, serializeArray = false))
-        jsonResponse(json, "Access-Control-Allow-Origin" -> "*",
-          "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers" -> "Content-Type, X-Requested-With, Accept",
-          // cache access control response for one day
-          "Access-Control-Max-Age" -> (60 * 60 * 24).toString)
+        scrapedData
       }
     } else {
-      Future.successful(jsonResponse(toJsObject(oldVal)))
+      oldVal
     }
+
+    val json = Json.obj("url" -> url, "data" -> toJsObject(data, serializeArray = false))
+    Future.successful(jsonResponse(json, "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "Content-Type, X-Requested-With, Accept",
+      // cache access control response for one day
+      "Access-Control-Max-Age" -> (60 * 60 * 24).toString))
   }
 }
