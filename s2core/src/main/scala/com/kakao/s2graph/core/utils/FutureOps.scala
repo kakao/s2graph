94cb7dc7c3974afb760e6220d3658c65758774ee
daewon <blueiur@gmail.com>
2015-11-04 20:38:17 +0900

    add ec

--- s2core/src/main/scala/com/kakao/s2graph/core/utils/FutureOps.scala
+++ s2core/src/main/scala/com/kakao/s2graph/core/utils/FutureOps.scala
@@ -1,10 +1,10 @@
 package com.kakao.s2graph.core.utils
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object FutureOps {
 
-  def retry[T](n: Int)(f: => Future[T]): Future[T] = {
+  def retry[T](n: Int)(f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
     n match {
       case i if i > 1 => f recoverWith { case t: Throwable => retry(n - 1)(f) }
       case _ =>
