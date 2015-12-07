412272b7fcf6aa2c0e3f5544d89c50ccb6af8f36
Jaesang Kim <honeysleep@gmail.com>
2015-12-07 19:19:50 +0900

    add a etl model class

---
+++ s2core/src/main/scala/com/kakao/s2graph/core/mysqls/Etl.scala
@@ -0,0 +1,71 @@
+package com.kakao.s2graph.core.mysqls
+
+import scala.util.Try
+import scalikejdbc._
+
+/**
+  * Created by hsleep(honeysleep@gmail.com) on 2015. 12. 7..
+  */
+case class Etl(id: Option[Int],
+               originalLabelId: Int,
+               transformLabelId: Int,
+               srcEtlQueryId: Option[Int],
+               tgtEtlQueryId: Option[Int],
+               propEtlQueryId: Option[Int])
+
+object Etl extends Model[Etl] {
+  val e = Etl.syntax("e")
+  val columnName = Etl.column
+
+  def apply(c: SyntaxProvider[Etl])(rs: WrappedResultSet): Etl = apply(c.resultName)(rs)
+  def apply(r: ResultName[Etl])(rs: WrappedResultSet): Etl = {
+    Etl(Option(rs.int(r.id)), rs.int(r.originalLabelId), rs.int(r.transformLabelId),
+      rs.intOpt(r.srcEtlQueryId), rs.intOpt(r.tgtEtlQueryId), rs.intOpt(r.propEtlQueryId))
+  }
+
+  def create(originalLabelId: Int,
+             transformLabelId: Int,
+             srcEtlQueryId: Option[Int] = None,
+             tgtEtlQueryId: Option[Int] = None,
+             propEtlQueryId: Option[Int] = None)
+            (implicit session: DBSession = AutoSession): Try[Etl] = {
+    Try {
+      val id = withSQL {
+        insert.into(Etl).namedValues(
+          columnName.originalLabelId -> originalLabelId,
+          columnName.transformLabelId -> transformLabelId,
+          columnName.srcEtlQueryId -> srcEtlQueryId,
+          columnName.tgtEtlQueryId -> tgtEtlQueryId,
+          columnName.propEtlQueryId -> propEtlQueryId
+        )
+      }.updateAndReturnGeneratedKey().apply().toInt
+      Etl(Some(id), originalLabelId, transformLabelId, srcEtlQueryId, tgtEtlQueryId, propEtlQueryId)
+    }
+  }
+
+  def findByOriginalLabelIds(originalLabelId: Int, useCache: Boolean = true)
+                            (implicit session: DBSession = AutoSession): List[Etl] = {
+    val cacheKey = s"originalLabelId=$originalLabelId"
+    val sql = withSQL {
+      selectFrom(Etl as e).where.eq(e.originalLabelId, originalLabelId)
+    }.map(Etl(e))
+
+    useCache match {
+      case true =>
+        withCaches(cacheKey) {
+          sql.list().apply()
+        }
+      case false =>
+        sql.list().apply()
+    }
+  }
+
+  def delete(id: Int)
+            (implicit session: DBSession = AutoSession): Try[Unit] = {
+    Try {
+      withSQL {
+        deleteFrom(Etl).where.eq(columnName.id, id)
+      }.update().apply()
+    }
+  }
+}
---
+++ s2core/src/test/scala/com/kakao/s2graph/core/mysqls/EtlSpec.scala
@@ -0,0 +1,118 @@
+package com.kakao.s2graph.core.mysqls
+
+import java.util.Properties
+
+import com.kakao.s2graph.core.Management
+import com.kakao.s2graph.core.Management.JsonModel.{Index, Prop}
+import com.kakao.s2graph.core.types.HBaseType
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+
+/**
+  * Created by hsleep(honeysleep@gmail.com) on 2015. 12. 7..
+  */
+class EtlSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+  val Ttl = 2
+
+  val etlLabelName = "test_label_etl"
+
+  val props = new Properties()
+  props.setProperty("cache.ttl.seconds", Ttl.toString)
+  val config = ConfigFactory.load(ConfigFactory.parseProperties(props))
+
+  val cluster = config.getString("hbase.zookeeper.quorum")
+  val serviceName = "_test_service"
+  val serviceNameV2 = "_test_service_v2"
+  val columnName = "user_id"
+  val columnNameV2 = "user_id_v2"
+  val columnType = "long"
+  val columnTypeV2 = "long"
+  val hTableName = "_test_cases"
+  val preSplitSize = 0
+  val labelName = "_test_label"
+
+  val testProps = Seq(
+    Prop("affinity_score", "0.0", "double"),
+    Prop("is_blocked", "false", "boolean"),
+    Prop("time", "0", "int"),
+    Prop("weight", "0", "int"),
+    Prop("is_hidden", "true", "boolean"),
+    Prop("phone_number", "xxx-xxx-xxxx", "string"),
+    Prop("score", "0.1", "float"),
+    Prop("age", "10", "int")
+  )
+  val testIdxProps = Seq(Index("_PK", Seq("_timestamp", "affinity_score")))
+  val hTableTTL = None
+
+  override def beforeAll: Unit = {
+    Model.apply(config)
+
+    Management.createService(serviceName, cluster, hTableName, preSplitSize, hTableTTL = None, "gz")
+    Label.findByName(labelName, useCache = false) match {
+      case Some(label) =>
+        Etl.findByOriginalLabelIds(label.id.get, useCache = false).foreach { e =>
+          Etl.delete(e.id.get)
+        }
+      case None =>
+        Management.createLabel(labelName, serviceName, columnName, columnType, serviceName, columnName, columnType,
+          isDirected = true, serviceName, testIdxProps, testProps, "weak", Some(hTableName), hTableTTL,
+          HBaseType.VERSION3, isAsync = false)
+    }
+    if (Label.findByName(labelName, useCache = false).isEmpty) {
+      Management.createLabel(labelName, serviceName, columnName, columnType, serviceName, columnName, columnType,
+        isDirected = true, serviceName, testIdxProps, testProps, "weak", Some(hTableName), hTableTTL,
+        HBaseType.VERSION3, isAsync = false)
+    }
+    if (Label.findByName(etlLabelName, useCache = false).isEmpty) {
+      Management.createLabel(etlLabelName, serviceName, columnName, columnType, serviceName, columnName, columnType,
+        isDirected = true, serviceName, testIdxProps, testProps, "weak", Some(hTableName), hTableTTL,
+        HBaseType.VERSION3, isAsync = false)
+    }
+  }
+
+  override def afterAll: Unit = {
+    Label.findByName(labelName, useCache = false).foreach { label =>
+      Etl.findByOriginalLabelIds(label.id.get, useCache = false).foreach { e =>
+        Etl.delete(e.id.get)
+      }
+      Label.delete(label.id.get)
+    }
+    Label.findByName(etlLabelName, useCache = false).foreach { label =>
+      Label.delete(label.id.get)
+    }
+  }
+
+  "Etl" should "create one on DB" in {
+    val originalLabel = Label.findByName(labelName)
+    originalLabel should not be empty
+
+    val etlLabel = Label.findByName(etlLabelName)
+    etlLabel should not be empty
+
+    val op = Etl.create(originalLabel.get.id.get, etlLabel.get.id.get, None, None, None)
+    if (op.isFailure) {
+      op.failed.map { ex => println(s"$ex")}
+    }
+    op.isSuccess should equal(true)
+  }
+
+  it should "find list by original label id" in {
+    val originalLabel = Label.findByName(labelName)
+    originalLabel should not be empty
+
+    val etlLabel = Label.findByName(etlLabelName)
+    etlLabel should not be empty
+
+    val etls = Etl.findByOriginalLabelIds(originalLabel.get.id.get)
+    etls should have size 1
+
+    etls foreach { etl =>
+      etl.originalLabelId should equal(originalLabel.get.id.get)
+      etl.transformLabelId should equal(etlLabel.get.id.get)
+    }
+  }
+
+  it should "find list by transform label id" in {
+
+  }
+}
